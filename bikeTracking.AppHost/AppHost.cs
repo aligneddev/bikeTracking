f33fb048d4ed84d17f9a0401d8faddbd9563a2bc
aligneddev <klogan@omnitech-inc.com>
2025-12-17 07:48:21 -0600

    Aspire and DB, ran migration manually, post doesn't work

--- bikeTracking.AppHost/AppHost.cs
+++ bikeTracking.AppHost/AppHost.cs
@@ -1,11 +1,29 @@
-﻿var builder = DistributedApplication.CreateBuilder(args);
+﻿using Aspire.Hosting.Publishing;
+
+var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
+// SQL Server Integration Reference:
+// https://aspire.dev/integrations/databases/sql-server/#add-sql-server-resource-and-database-resource
+
+//var sqlPassword = builder.AddParameter("sql-password", new SqlPasswordDefault());    // password can be overridden in user secrets or environment variables
+
+// the EF migrations are not ran automatically, dotnet ef database update --connection "Server=127.0.0.1,1433;User ID=sa;Password=;Initial Catalog=BikeTrackingDb
+
+#pragma warning disable ASPIREPROXYENDPOINTS001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
 var sql = builder.AddSqlServer("sql")
-                 .AddDatabase("BikeTrackingDb");
+                .WithImageTag("2025-latest")
+                .WithContainerName("biketracking-sql")
+                .WithDataVolume("biketracking-sql-data")
+                .WithLifetime(ContainerLifetime.Persistent)
+                .WithEndpointProxySupport(false);
+#pragma warning restore ASPIREPROXYENDPOINTS001
+
+var db = sql.AddDatabase("BikeTrackingDb");
 var apiService = builder.AddProject<Projects.bikeTracking_ApiService>("apiservice")
-    .WithReference(sql)
+    .WithReference(db)
+    .WaitFor(db)
     .WithReference(cache)
     .WaitFor(cache);
 
